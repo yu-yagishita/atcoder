2b10447b2f791201b9fad2f565adec8275692b2e
yu-yagishita <yu.yagishita1995@gmail.com>
2021-01-27 10:22:45 +0900

    refactor(arihon): 理解を深めた

--- ant_book_beginner/example_2_1_1/abc104_c.py
+++ ant_book_beginner/example_2_1_1/abc104_c.py
@@ -1,48 +1,35 @@
-# D, G = map(int, input().split())
-# A = [[int(_) for _ in input().split()] for i in range(D)]
-# hantei = D
-# ans = 0
-# for i in range(len(A)):
-#     ans += A[i][0]
-
-# for i in range(2**hantei):
-#     b = [0]*hantei
-#     for j in range(hantei):
-#         if ((i >> j) & 1):
-#             b[hantei-1-j] = 1
-#     for k in range(len(b)):
-#         tensu = 0
-#         if b[k]==1:
-#             tensu += (k+1)*100*A[k][0]+A[k][1]
-#         if tensu>=G and ans>A[k][0]:
-#             ans = A[k][0]
-# print(ans)
-
 D, G = map(int, input().split())
 G //= 100
 l = []
 for i in range(D):
-  p, c = map(int, input().split())
-  tmp = [i+1, p, (i+1)*p+(c//100)]
-  l.append(tmp)
+    p, c = map(int, input().split())
+    tmp = [i+1, p, (i+1)*p+(c//100)]
+    l.append(tmp)
 
 ans = []
+
+# * これがbit探索を表現している
 for i in range(2**D):
-  tmp, cnt = 0, 0
-  flag = True
-  for j in range(D):
-    if (i>>j) & 1:
-      tmp += l[-j-1][2]
-      cnt += l[-j-1][1]
-    else:
-      if flag:
-        mx = D-j
-        flag = False
-  if tmp >= G:
-    ans.append(cnt)
-  elif tmp+mx*(l[mx-1][1]-1) >= G:
-    cnt += (G-tmp) // mx
-    if (G-tmp) % mx != 0:
-      cnt += 1
-    ans.append(cnt)
+    tmp, cnt = 0, 0
+    flag = True
+    for j in range(D):
+        if (i >> j) & 1:
+            # * 全問正解したときの値を設定している。
+            # * tmp:得点, cnt:問題数
+            tmp += l[-j-1][2]
+            cnt += l[-j-1][1]
+        else:
+            if flag:
+                # * 全問正解ではなくGに達するために点数が低い問題に取り組んでいる際の何点問題に取り組んでいるかを表している
+                mx = D-j
+                flag = False
+    # * 全問正解でGを超えるかどうか
+    if tmp >= G:
+        ans.append(cnt)
+    # * 中途半端に問題を解いてGを超えるかどうか
+    elif tmp+mx*(l[mx-1][1]-1) >= G:
+        cnt += (G-tmp) // mx
+        if (G-tmp) % mx != 0:
+            cnt += 1
+        ans.append(cnt)
 print(min(ans))
